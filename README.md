9c5bb9773f0f5f336a6e6a3fa67332e792d250df
mcbal <matthiascbal@gmail.com>
2021-09-04 18:29:37 +0200

    Add scaffolding

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Approximate free-energy minimization
+# Approximate Free-Energy Minimization
+
+Experimental implementation of approximate free-energy minimization in PyTorch.
+
+**Summary:**
+
+**Blog post:**
+
+## References
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='afem',
+    packages=find_packages(exclude=['images']),
+    version='0.0.0',
+    license='MIT',
+    description='Approximate Free-Energy Minimization',
+    author='Matthias Bal',
+    author_email='matthiascbal@gmail.com',
+    url='https://github.com/mcbal/afem',
+    keywords=['artificial intelligence', 'attention mechanism'],
+    install_requires=['einops>=0.3', 'numpy>=1.19', 'torch>=1.8'],
+)
