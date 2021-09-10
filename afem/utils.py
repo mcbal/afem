f522eeb8bd6a54647d2997e6765d11db9c0886d0
mcbal <matthiascbal@gmail.com>
2021-09-10 10:57:07 +0200

    Clean utils

--- afem/utils.py
+++ afem/utils.py
@@ -13,14 +13,25 @@ def set_all_seeds(seed: int):
     torch.cuda.manual_seed_all(seed)
 
 
-def remove_kwargs(kwargs, prefix):
-    """Remove kwargs starting with `prefix` from dict."""
-    return {k: v for k, v in kwargs.items() if not k.startswith(prefix)}
+def batched_jacobian_for_vector_outputs(f, x, create_graph=False):
+    """
+    https://discuss.pytorch.org/t/jacobian-functional-api-batch-respecting-jacobian/84571
+    """
+    def _f_sum(x):
+        return f(x).sum(dim=0)
+    return autograd.functional.jacobian(_f_sum, x, create_graph=create_graph).permute(1, 0, 2)
 
 
-def filter_kwargs(kwargs, prefix):
-    """Keep only kwargs starting with `prefix` and strip `prefix` from keys of kept items."""
-    return {k.replace(prefix, ''): v for k, v in kwargs.items() if k.startswith(prefix)}
+def batched_eye(bsz: int, dim: int, device, dtype):
+    """Return batch of identity matrices."""
+    return torch.eye(dim, device=device, dtype=dtype)[None, :, :].repeat(
+        bsz, 1, 1
+    )
+
+
+def batched_eye_like(X: torch.Tensor):
+    """Return batch of identity matrices like given batch of matrices `X`."""
+    return torch.eye(*X.shape[1:], out=torch.empty_like(X))[None, :, :].repeat(X.shape[0], 1, 1)
 
 
 def exists(val):
@@ -33,33 +44,11 @@ def default(val, d):
     return d() if isfunction(d) else d
 
 
-def batched_eye(bsz: int, dim: int, device, dtype):
-    return torch.eye(dim, device=device, dtype=dtype)[None, :, :].repeat(
-        bsz, 1, 1
-    )
+def remove_kwargs(kwargs, prefix):
+    """Remove kwargs starting with `prefix` from dict."""
+    return {k: v for k, v in kwargs.items() if not k.startswith(prefix)}
 
 
-def batched_eye_like(X: torch.Tensor):
-    return torch.eye(*X.shape[1:], out=torch.empty_like(X))[None, :, :].repeat(
-        X.shape[0], 1, 1
-    )
-
-
-def batched_jacobian_for_scalar_fun(f, x, create_graph=False):
-    """
-    https://discuss.pytorch.org/t/jacobian-functional-api-batch-respecting-jacobian/84571/6
-    https://discuss.pytorch.org/t/computing-batch-jacobian-efficiently/80771/4
-    """
-    def _f_sum(x):
-        return f(x).sum(dim=0)
-    return autograd.functional.jacobian(_f_sum, x, create_graph=create_graph)
-
-
-def batched_jacobian_for_vector_fun(f, x, create_graph=False):
-    """
-    https://discuss.pytorch.org/t/jacobian-functional-api-batch-respecting-jacobian/84571/6
-    https://discuss.pytorch.org/t/computing-batch-jacobian-efficiently/80771/4
-    """
-    def _f_sum(x):
-        return f(x).sum(dim=0)
-    return autograd.functional.jacobian(_f_sum, x, create_graph=create_graph)  # .permute(1, 0, 2)
+def filter_kwargs(kwargs, prefix):
+    """Keep only kwargs starting with `prefix` and strip `prefix` from keys of kept items."""
+    return {k.replace(prefix, ''): v for k, v in kwargs.items() if k.startswith(prefix)}
