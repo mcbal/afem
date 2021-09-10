Fix sign in rootfind and clean up

afem/rootfind.py
@@ -34,7 +34,7 @@ class RootFind(nn.Module):
 
         if self.training:
             # Re-engage autograd tape (no-op in terms of value of z).
-            new_z_root = z_root - self.fun(z_root.requires_grad_(), x,
+            new_z_root = z_root + self.fun(z_root.requires_grad_(), x,
                                            *args, **remove_kwargs(kwargs, 'solver_'))
 
             # Set up backward hook for root-solving in backward pass.
@@ -42,19 +42,12 @@ class RootFind(nn.Module):
             fun_bwd = self.fun(z_bwd, x, *args, **remove_kwargs(kwargs, 'solver_'))
 
             def backward_hook(grad):
-                print(f'inside grad {grad}')
+                return self.solver(
+                    lambda y: autograd.grad(fun_bwd, z_bwd, y, retain_graph=True, create_graph=True)[0] + grad,
+                    torch.zeros_like(grad), **filter_kwargs(kwargs, 'solver_bwd_')
+                )['result']
 
-                new_grad = self.solver(lambda y: autograd.grad(fun_bwd, z_bwd, y, retain_graph=True, create_graph=True)[
-                                       0] + grad, torch.zeros_like(grad), **filter_kwargs(kwargs, 'solver_bwd_'), )['result']
-
-                # new_grad = torch.linalg.solve(kwargs['solver_bwd_grad_f'](z_bwd), grad)
-
-                # new_grad = self.solver(lambda y: autograd.grad(fun_bwd, z_bwd, y, retain_graph=True, create_graph=True)[
-                #                        0] + grad, torch.zeros_like(grad), **filter_kwargs(kwargs, 'solver_bwd_'), )['result']
-                print(f'inside new_grad {new_grad}')
-                return new_grad
-
-            new_z_root.register_hook(backward_hook)  # kwargs.get('grad_f', None) is not None
+            new_z_root.register_hook(backward_hook)
 
         return new_z_root
 