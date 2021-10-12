8a55ff26befdde892b083d52e08529222a0b54b6
mcbal <matthiascbal@gmail.com>
2021-10-12 11:32:49 +0200

    Add model examples

---
+++ examples/model_beta_sweep.py
@@ -0,0 +1,84 @@
+# Run forward and backward pass on a `VectorSpinAttention` module and plot `phi` and its derivatives.
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import tqdm
+from matplotlib import animation
+
+from afem.models import VectorSpinModel
+
+num_spins, dim = 32, 128
+beta_min, beta_max, beta_num_steps = -0.6, 1.0, 100  # log10-space
+plot_values_cutoff = 500
+
+x = (torch.randn(1, num_spins, dim) / np.sqrt(dim)).requires_grad_()
+t0 = 0.5*torch.ones(1)
+
+
+def filter_array(a, threshold=plot_values_cutoff):
+    idx = np.where(np.abs(a) > threshold)
+    a[idx] = np.nan
+    return a
+
+
+def simple_update(frame, fig, axs):
+
+    print(f'{frame:.4f} / {10**beta_max}')
+
+    # Setup vector-spin model.
+    model = VectorSpinModel(
+        num_spins=num_spins,
+        dim=dim,
+        beta=frame,
+    )
+    out = model(x, t0=t0, return_afe=True, return_magnetizations=True)
+
+    t_star = out.t_star[0][0].detach().numpy()
+    afe_star = out.afe[0][0].detach().numpy()
+    t_min, t_max, t_step = 0.0, 3.0, 0.001
+    t_range = torch.arange(t_min, t_max, t_step)[:, None]
+    phis = np.array(model._phi(t_range, x[:1, :, :].repeat(t_range.numel(), 1, 1)).detach().numpy())
+    grad_phis = np.array(model._jac_phi(
+        t_range, x[:1, :, :].repeat(t_range.numel(), 1, 1)).detach().numpy())
+    grad_grad_phis = np.array(model._hess_phi(
+        t_range, x[:1, :, :].repeat(t_range.numel(), 1, 1)).detach().numpy()).squeeze()
+
+    axs[0].clear()
+    axs[0].set_title(
+        f"beta = {frame:.4f}, t = {t_star:.4f}, afe = {afe_star:.4f}")
+    axs[0].plot(t_range.numpy().squeeze(), filter_array(phis), 'r-')
+    axs[0].axvline(x=out.t_star[0].detach().numpy())
+    axs[0].set_ylim([-plot_values_cutoff, plot_values_cutoff])
+    axs[0].set_ylabel("phi(t)")
+
+    axs[1].clear()
+    axs[1].plot(t_range.numpy().squeeze(), filter_array(grad_phis), 'r-')
+    axs[1].axvline(x=out.t_star[0].detach().numpy())
+    axs[1].axhline(y=0.0)
+    axs[1].set_ylim([-plot_values_cutoff, 100])
+    axs[1].set_ylabel("phi'(t)")
+
+    axs[2].clear()
+    axs[2].plot(t_range.numpy().squeeze(), filter_array(grad_grad_phis, 2*plot_values_cutoff), 'r-')
+    axs[2].axvline(x=out.t_star[0].detach().numpy())
+    axs[2].set_xlim([t_min, t_max])
+    axs[2].set_xlabel('t')
+    axs[2].set_ylim([0, 2*plot_values_cutoff])
+    axs[2].set_ylabel("phi''(t)")
+
+    fig.align_ylabels(axs)
+
+
+def simple_animation():
+    fig, axs = plt.subplots(3, 1, sharex=True)
+    ani = animation.FuncAnimation(
+        fig, simple_update, frames=np.logspace(beta_min, beta_max, beta_num_steps),
+        fargs=(fig, axs,),
+        interval=100,
+        repeat_delay=100,
+    )
+    ani.save('animation.gif', writer='imagemagick')
+
+
+simple_animation()
--- examples/model_fwd_bwd.py
+++ examples/model_fwd_bwd.py
@@ -4,30 +4,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 
-from afem.attention import VectorSpinAttention
+from afem.models import VectorSpinModel
 
-num_spins, dim = 11, 17
+num_spins, dim = 32, 128
 
-# Setup model.
-attention = VectorSpinAttention(
+# Setup vector-spin model.
+model = VectorSpinModel(
     num_spins=num_spins,
     dim=dim,
-    pre_norm=False,  # otherwise plots will not match up (normalizing happens inside `VectorSpinAttention`)
     beta=1.0,
-    J_add_external=True,
-    J_symmetric=True,
-    J_traceless=True,
 )
 
 # Create random inputs and initial value for auxiliary variable (see `afem.models.VectorSpinModel`).
-x = torch.randn(1, num_spins, dim).requires_grad_()
+x = (torch.randn(1, num_spins, dim) / np.sqrt(dim)).requires_grad_()
 t0 = torch.ones(1)
 
 # Run forward pass and get output magnetizations, stationary t_star value, and approximate free energy.
-out = attention(x, t0=t0, return_afe=True, return_magnetizations=True)
+out = model(x, t0=t0, return_afe=True, return_magnetizations=True)
 
 print(
-    f'✨ t_star {out.t_star.detach()}\n✨ magnetizations: {out.magnetizations}\n✨ approximate free energy: {out.afe.detach()}'
+    f'✨ magnetizations: {out.magnetizations}\n✨ t_star {out.t_star.detach()}\n✨ approximate free energy: {out.afe.detach()}'
 )
 
 # Run backward on sum of free energies across batch dimension.
@@ -37,34 +33,55 @@ out.afe.sum().backward()
 # Plot internally-used function `phi(t)` and its derivatives for first element in batch. #
 #########################################################################################
 
-if t0.numel() == x.size(0):
+if x.size(0) == 1 and t0.numel() == 1:
 
-    def filter_array(a, threshold=1e2):
+    def filter_array(a, threshold=250):
         idx = np.where(np.abs(a) > threshold)
         a[idx] = np.nan
         return a
 
+    t_star = out.t_star[0][0].detach().numpy()
+    afe_star = out.afe[0][0].detach().numpy()
+    grad_phi_star = np.array(model._jac_phi(out.t_star, x[:1, :, :]).detach().numpy())
     # Pick a range and resolution for `t`.
-    t_range = torch.arange(0.0, 3.0, 0.0001)[:, None]
+    t_min, t_max, t_step = 0.0, 3.0, 0.001
+    t_range = torch.arange(t_min, t_max, t_step)[:, None]
     # Calculate function evaluations for every point on grid and plot.
-    phis = np.array(attention.spin_model._phi(t_range, x[:1, :, :].repeat(t_range.numel(), 1, 1)).detach().numpy())
-    grad_phis = np.array(attention.spin_model._jac_phi(
+    phis = np.array(model._phi(t_range, x[:1, :, :].repeat(t_range.numel(), 1, 1)).detach().numpy())
+    grad_phis = np.array(model._jac_phi(
         t_range, x[:1, :, :].repeat(t_range.numel(), 1, 1)).detach().numpy())
-    grad_grad_phis = np.array(attention.spin_model._hess_phi(
+    grad_grad_phis = np.array(model._hess_phi(
         t_range, x[:1, :, :].repeat(t_range.numel(), 1, 1)).detach().numpy()).squeeze()
-    f, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True)
-    ax1.set_title(f"Found root of phi'(t) at t = {out.t_star[0][0].detach().numpy():.4f}")
-    ax1.plot(t_range.numpy().squeeze(), filter_array(phis), 'r-')
-    ax1.axvline(x=out.t_star[0].detach().numpy())
-    ax1.set_ylabel("phi(t)")
-    ax2.plot(t_range.numpy().squeeze(), filter_array(grad_phis), 'r-')
-    ax2.axvline(x=out.t_star[0].detach().numpy())
-    ax2.axhline(y=0.0)
-    ax2.set_ylabel("phi'(t)")
-    ax3.plot(t_range.numpy().squeeze(), filter_array(grad_grad_phis), 'r-')
-    ax3.axvline(x=out.t_star[0].detach().numpy())
-    ax3.axhline(y=0.0)
-    ax3.set_xlim([0, 3])
-    ax3.set_xlabel('t')
-    ax3.set_ylabel("phi''(t)")
+
+    fig, axs = plt.subplots(3, 1, sharex=True)
+
+    axs[0].set_title(
+        f"Found root of phi'(t) at t = {t_star:.4f} (afe = {afe_star:.4f})")
+    axs[0].plot(t_range.numpy().squeeze(), filter_array(phis), 'r-')
+    axs[0].axvline(x=out.t_star[0].detach().numpy())
+    axs[0].set_ylabel("phi(t)")
+
+    axs[1].plot(t_range.numpy().squeeze(), filter_array(grad_phis), 'r-')
+    axs[1].axvline(x=out.t_star[0].detach().numpy())
+    axs[1].axhline(y=0.0)
+    axs1_inset = axs[1].inset_axes([0.45, 0.20, 0.47, 0.47])
+    axs1_inset.plot(t_range.numpy().squeeze(), filter_array(grad_phis), 'r-')
+    axs1_inset.axvline(x=out.t_star[0].detach().numpy())
+    axs1_inset.axhline(y=0.0)
+    x1, x2, y1, y2 = t_star-0.05, t_star+0.05, grad_phi_star-5, grad_phi_star+5
+    axs1_inset.set_xlim(x1, x2)
+    axs1_inset.set_ylim(y1, y2)
+    axs1_inset.set_xticklabels('')
+    axs1_inset.set_yticklabels('')
+    axs[1].indicate_inset_zoom(axs1_inset, edgecolor="black")
+    axs[1].set_ylabel("phi'(t)")
+
+    axs[2].plot(t_range.numpy().squeeze(), filter_array(grad_grad_phis), 'r-')
+    axs[2].axvline(x=out.t_star[0].detach().numpy())
+    axs[2].set_xlim([t_min, t_max])
+    axs[2].set_xlabel('t')
+    axs[2].set_ylabel("phi''(t)")
+
+    fig.align_ylabels(axs)
+    # plt.savefig('plot.png', bbox_inches='tight')
     plt.show()
